7b52e2d17075c57a7dd00a66adcb70e72668d812
senda287 <100978613+senda287@users.noreply.github.com>
2022-09-17 00:24:48 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,32 @@
 var salarySum=0;
 var salary=0;
+var startYear;
+var startMonth;
+var startDay;
+var startHour;
+var startMinute;
+var start;
+var nowTime;
 function displayBtn(){
-  const finDate = document.getElementById("inputStartDate").value; //YYYY-MM-DD
-  const finTime = document.getElementById("inputStartTime").value; //HH-MM-SS
-  const salary = document.getElementById("inputSalary").value;
-
-  document.getElementById("outputStartDate").textContent = finDate;
-  document.getElementById("outputStartTime").textContent = finTime;
-  document.getElementById("outputSalary").textContent = salary;
+  getStartTime();
   calcSalary();
-  setInterval(countSalary,1000);
+
+  displaySalary();
+  setInterval(displaySalary,1000);
+}
+
+function getStartTime(){
+  //入力内容の取得
+  const startDate = document.getElementById("inputStartDate").value; //YYYY-MM-DD
+  const startTime = document.getElementById("inputStartTime").value; //HH-MM-SS
+  
+  //開始時間を取得
+  startYear = Number(startDate.substr(0,4));
+  startMonth = Number(startDate.substr(5,2));
+  startDay = Number(startDate.substr(8,2));
+  startHour = Number(startTime.substr(0,2));
+  startMinute = Number(startTime.substr(3,2));
+  start = new Date(startYear,startMonth-1,startDay,startHour,startMinute);
 }
 
 function calcSalary(){
@@ -18,8 +35,18 @@ function calcSalary(){
   salary = Math.floor(salary * 100) / 100;
 }
 
-function countSalary(){
-  salarySum += salary;
+function displaySalary(){
+  const nowTime = new Date(); //現在時間の取得
+  const diff =  nowTime.getTime() - start.getTime(); // 開始時間からの経過を取得（ミリ秒）
 
-  document.getElementById("outputSalary").textContent = salarySum;
+  // ミリ秒から単位を修正
+  const calcHour = Math.floor(diff / 1000 / 60 / 60);
+  const calcMin = Math.floor(diff / 1000 / 60) % 60;
+  const calcSec = Math.floor(diff / 1000) % 60;
+  const sec = Math.floor(diff / 1000);
+
+  document.getElementById("outputHour").textContent = calcHour;
+  document.getElementById("outputMin").textContent = calcMin;
+  document.getElementById("outputSec").textContent = calcSec;
+  document.getElementById("outputSalary").textContent = sec * salary;
 }
